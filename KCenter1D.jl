9f688304f1f3ddc48fca63a29ea0e1c2b0f761be
shkr <shashank.f1@gmail.com>
2014-06-19 12:17:49 -0400

    Julia KCenter1D debugged

--- KCenter1D.jl
+++ KCenter1D.jl
@@ -1,9 +1,6 @@
-# in KCenter1D.jl
+#in KCenter1D.jl
 
-y  = [0.1,0.2,0.3]
-K  = 1
-
-function KCenter1D(y::Array{Float64,1},K::Int64):
+function KCenter1D(y::Array{Float64,1},K::Int64)
 
 	# Description
 	# -----------
@@ -15,28 +12,31 @@ function KCenter1D(y::Array{Float64,1},K::Int64):
 	# y -- a vector of sorted numbers
 	# K -- the number of clusters expected
 
-	# Pre-Conditions
+	#Trivial case
 	# -----------
-	# K <= |set(y)|
+	# K > |set(y)|
+	# |set(y)| == 1
+	if (K>length(unique(y)) || length(unique(y))==1)
+		return (ASCIIString=>Array{Float64})["Cluster"=>ones(Int32,length(y)),"Centers"=>y,"Size"=>ones(Int32,K),"WithinSS"=>zeros(Float64,length(y))]
+	end
+
+	# Assumptions
+	# -----------
+	# K < |set(y)|
 	# |set(y)| > 1
 
-	
-
-	if (K>length(unique(y)) || length(set(y))==1)
-		return (ASCIIString=>Float64)["Cluster"=>fill(1,length(y)),"Centers"=>y,"Size"=>fill(1,K)]
-	end
 
 	N = length(y) # N: is the size of input vector
 	
 
 	#D - Matrix rows 1 to K, columns 1 to N, OPTIMAL Solution WithinSS for KCenter(y,K)
-	D = zeros(Float64,(K,N))
+	D = Array(Float64,(K,N))
 	#B - Matrix rows 1 to K, columns 1 to N, Position of leftmost element of Kth cluster of OPTIMAL Solution
-	B = ones(Float64,(K,N))
+	B = Array(Float64,(K,N))
 
 	#Base case : Set for all K rows, Column 1 to 0
-	#B[1:K,1] = 1.0
-	#D[1:K,1] = 0.0
+	B[1:K,1] = 1.0
+	D[1:K,1] = 0.0
 	
 	# running values
 	# μ_y1     :     mean of when k=1
@@ -49,19 +49,19 @@ function KCenter1D(y::Array{Float64,1},K::Int64):
 
 		for i=2:N
 
-			# when k = 1, D(1,i) = d(1,i)
+			# when k = 1, D(1,i) = d(1 to i)
 			if (k == 1)
 
-				D(1,i) = D(1,i-1) + ((float64(i-1)/float64(i))*(y[i]-μ_y1)^2)
+				D[1,i] = D[1,i-1] + ((float64(i-1)/float64(i))*(y[i]-μ_y1)^2)
 				μ_y1   = (float64(i-1)*μ_y1 + y[i])/float64(i)
-				B(1,i) = 1
+				B[1,i] = 1
 
 			#when k > 1, D(k,i)  = min ( D(k-1,j-1) + d(j,i) ) for j>=1
 			else
 
 				d      = 0.0   			#Initiallly no item in rightmost-cluster
 				μ_yj   = 0.0	 		#Initiallly no item in rightmost-cluster
-				D(k,i) = None  			#value of D(k,i) is not set
+				D[k,i] = -1  			#value of D(k,i) is not set
 
 				for j in i:-1:1
 
@@ -70,25 +70,25 @@ function KCenter1D(y::Array{Float64,1},K::Int64):
 					μ_yj = (y[j] + float64(i-j)*μ_yj) / float64(i-j+1)	   	   #update mean(j,i)
 
 					#initialize D(k,i) with D(k,i) = D(k-1,i-1) + d
-					if (D(k,i) == -1)
+					if (D[k,i] == -1)
 						if (j == 1)
-							D(k,i) = d
-							B(k,i) = float64(j)
-						else:
-							D(k,i) = d + D(k-1,j-1)
-							B(k,i) = float64(j)
+							D[k,i] = d
+							B[k,i] = float64(j)
+						else
+							D[k,i] = d + D[k-1,j-1]
+							B[k,i] = float64(j)
 						end
 					#If already initialized find if D[k-1][j-1] + d(j,i) < D[k][i] OR d(1,i) < D[k][i]
 					else
 						if (j == 1)
-							if d <= D(k,i)
-								D(k,i) = d
-								B(k,i) = float64(j)
+							if d <= D[k,i]
+								D[k,i] = d
+								B[k,i] = float64(j)
 							end
 						else
-							if (d+D(k-1,j-1) < D(k,i))
-								D(k,i) = d + D(k-1,j-1)
-								B(k,i) = float64(j)
+							if (d+D[k-1,j-1] < D[k,i])
+								D[k,i] = d + D[k-1,j-1]
+								B[k,i] = float64(j)
 							end
 						end
 
@@ -105,16 +105,16 @@ function KCenter1D(y::Array{Float64,1},K::Int64):
 	#Backtrack to find the clusters of the data points
 	cluster_right = N
 	cluster_left  = -1
-	Cluster 			= Array{Int64}(N)   # record which cluster each point belongs to
-	Center   			= Array{Float64}(K)	 # record the center of each cluster
-	WithinSS 		 	= Array{Float64}(K)   # within sum of distance square of each cluster
-	Size 				= Array{Float64}(K)	 # size of each cluster
+	Cluster 			= Array(Int32,N)     # record which cluster each point belongs to
+	Center   			= Array(Float64,K)	 # record the center of each cluster
+	WithinSS 		 	= Array(Float64,K)   # within sum of distance square of each cluster
+	Size 				= Array(Float64,K)	 # size of each cluster
 
 	for k=K:-1:1
 
-		cluster_left = B(k,cluster_right)
+		cluster_left = B[k,cluster_right]
 
-		for i=cluster_left:cluster_right+1
+		for i=cluster_left:cluster_right
 			Cluster[i] = k
 		end
 
@@ -126,11 +126,11 @@ function KCenter1D(y::Array{Float64,1},K::Int64):
 
 		Center[k] = ss / (float64(cluster_right) - cluster_left + 1)
 
-		for a=cluster_left:cluster_right+1
+		for a=cluster_left:cluster_right
 			WithinSS[k] += (y[a] - Center[k])^2
 		end
 
-		Size[k] = cluster_right - Int64(cluster_left) + 1
+		Size[k] = cluster_right - int(cluster_left) + 1
 
 		if (k > 1)
 			cluster_right = int(cluster_left) - 1
@@ -138,5 +138,5 @@ function KCenter1D(y::Array{Float64,1},K::Int64):
 	
 	end
 
-	return (ASCIIString=>Float64)["Cluster"=>Clusters,"Centers"=>Center,"Size"=>Size,"WithinSS"=>WithinSS]
+	return (ASCIIString=>Array{Float64})["Cluster"=>Cluster,"Centers"=>Center,"Size"=>Size,"WithinSS"=>WithinSS]
 end
